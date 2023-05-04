11cb1eada50b510dadad7fa5b266d5d7bc4a0e90
Mircea N <nunu.mircea@gmail.com>
2023-05-04 00:37:04 -0400

    Learner model
    Signed-off-by: Mircea N <nunu.mircea@gmail.com>

--- crud/models.py
+++ crud/models.py
@@ -25,6 +25,34 @@ class Instructor(User):
                "Is full time: " + str(self.full_time) + ", " + \
                "Total Learners: " + str(self.total_learners)
 
+# Learner model
+class Learner(User):
+    STUDENT = 'student'
+    DEVELOPER = 'developer'
+    DATA_SCIENTIST = 'data_scientist'
+    DATABASE_ADMIN = 'dba'
+    OCCUPATION_CHOICES = [
+        (STUDENT, 'Student'),
+        (DEVELOPER, 'Developer'),
+        (DATA_SCIENTIST, 'Data Scientist'),
+        (DATABASE_ADMIN, 'Database Admin')
+    ]
+    occupation = models.CharField(
+        null=False,
+        max_length=20,
+        choices=OCCUPATION_CHOICES,
+        default=STUDENT
+    )
+    social_link = models.URLField(max_length=200)
+    
+    ##<HINT> Create a __str__ method returning a string presentation
+    def __str__(self):
+        return "First name: " + self.first_name + ", " + \
+               "Last name: " + self.last_name + ", " + \
+                "Date of Birth: " + str(self.dob) + ", " + \
+                "Occupation: " + self.occupation + ", " + \
+                "Social Link: " + self.social_link
+
 # Course model
 class Course(models.Model):
     name = models.CharField(null=False, max_length=100, default='online course')
