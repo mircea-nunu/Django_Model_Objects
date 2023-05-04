8c9431ba6973ce161a7b08988b811599e8836b56
Mircea N <nunu.mircea@gmail.com>
2023-05-04 00:26:25 -0400

    models 1
    Signed-off-by: Mircea N <nunu.mircea@gmail.com>

--- crud/models.py
+++ crud/models.py
@@ -3,3 +3,43 @@ from django.utils.timezone import now
 
 # Define your models from here:
 
+# User model
+class User(models.Model):
+    first_name = models.CharField(null=False, max_length=30, default='john')
+    last_name = models.CharField(null=False, max_length=30, default='doe')
+    dob = models.DateField(null=True)
+    
+    # Create a toString method for object string representation
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+# Instructor model
+class Instructor(User):
+    full_time = models.BooleanField(default=True)
+    total_learners = models.IntegerField()
+    
+    # Create a toString method for object string representation
+    def __str__(self):
+        return "First name: " + self.first_name + ", " + \
+               "Last name: " + self.last_name + ", " + \
+               "Is full time: " + str(self.full_time) + ", " + \
+               "Total Learners: " + str(self.total_learners)
+
+# Course model
+class Course(models.Model):
+    name = models.CharField(null=False, max_length=100, default='online course')
+    description = models.CharField(max_length=500)
+    # Many-To-Many relationship with Instructor
+    instructors = models.ManyToManyField(Instructor)
+    
+    # Create a toString method for object string representation
+    def __str__(self):
+        return "Name: " + self.name + "," + \
+            "Description: " + self.description
+
+# Lesson
+class Lesson(models.Model):
+    title = models.CharField(max_length=200, default="title")
+    course = models.ForeignKey(Course, null=True, on_delete=models.CASCADE)
+    content = models.TextField()
+
